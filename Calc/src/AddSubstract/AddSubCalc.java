e7f197b261a3b0e1e018f8c960494158ed17c2ba
emeraldsingh <singhe@warhawks.ulm.edu>
2022-02-25 14:50:08 -0500

    Addition

---
+++ Calc/src/AddSubstract/AddSubCalc.java
@@ -0,0 +1,15 @@
+package AddSubstract;
+
+public class AddSubCalc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(4,5));
+	}
+
+	private static int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+}
