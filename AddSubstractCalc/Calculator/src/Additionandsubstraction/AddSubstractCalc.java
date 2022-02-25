0e0f803cbd81af11990946116fb1daef8125b019
emeraldsingh <singhe@warhawks.ulm.edu>
2022-02-25 14:08:01 -0500

    Add

---
+++ AddSubstractCalc/Calculator/src/Additionandsubstraction/AddSubstractCalc.java
@@ -0,0 +1,14 @@
+package Additionandsubstraction;
+
+public class AddSubstractCalc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(4,5));
+	}
+
+	public static int add(int a, int b) {
+		return a+b;
+		
+	}
+}
